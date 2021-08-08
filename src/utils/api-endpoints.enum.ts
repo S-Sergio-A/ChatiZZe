e580d7af628f1b02613755814972a3ef44c6a458
Sergey <sergios33033@gmail.com>
2021-08-08 18:48:33 +0300

    The list of all API endpoints.

--- src/utils/api-endpoints.enum.ts
+++ src/utils/api-endpoints.enum.ts
@@ -1,31 +1,40 @@
-const REST_API = "";
-
-export const publicLinks = {
-  articles: (language: string) => `${REST_API}/${language}/press/all`,
-  pressRelease: (language: string, id: string) => `${REST_API}/${language}/press/id/${id}`
-};
-
 export const clientLinks = {
-  analytics: `${REST_API}/analytics`,
-  createSession: `${REST_API}/client/create-session`,
-  contactUs: `${REST_API}/client/contact`,
-  subscribe: `${REST_API}/client/subscribe`
+  invoke: "/invoke",
+  getToken: "/token",
+  contactUs: "/contact",
 };
 
 export const userLinks = {
-  changeOptionalData: `${REST_API}/user/optional-data`,
-  changeEmail: `${REST_API}/user/change-email`,
-  changePassword: `${REST_API}/user/change-password`,
-  changeTelNum: `${REST_API}/user/change-tel-num`,
-  contactUs: `${REST_API}/user/contact`,
-  loadChats: `${REST_API}/user/chats`,
-  loadMessages: (chatId: string) => `${REST_API}/user/chats/${chatId}`,
-  loadChatMetadata: (chatId: string) => `${REST_API}/user/chats/metadata/${chatId}`,
-  searchChannel: (keyword: string) => `${REST_API}/user/channel/${keyword}`,
-  searchMessage: (chatName: string, keyword: string) => `${REST_API}/user/${chatName}/messages/${keyword}`,
-  error: `${REST_API}/user/error/`,
-  login: `${REST_API}/user/login`,
-  refresh: `${REST_API}/user/refresh`,
-  register: `${REST_API}/user/register`,
-  validate: `${REST_API}/user/validate`
+  registration: "/sign-up",
+  verifyRegistration: (email: string, verification: string) => `/verify-registration?email=${email}&verification=${verification}`,
+  login: "/login",
+  resetPassword: "/reset-password",
+  verifyPasswordReset: (email: string) => `/verify-password-reset?email=${email}`,
+  logout: "/logout",
+
+  changeEmail: "/email",
+  changeUsername: "/username",
+  changePhone: "/phone",
+  changePassword: "/password",
+  verifyChange: (verification: string, dataType: string) => `/verify-change?verification=${verification}&dataType=${dataType}`,
+  changeOptionalData: "/optional",
+  changePhoto: "/photo",
+
+  refresh: "/refresh-session",
+
+  createRoom: "/create-room",
+  loadRooms: "/rooms",
+  loadUserRooms: "/user-rooms",
+  searchRooms: (name: string) => `/room/${name}`,
+  updateRoom: (userId: string, roomId: string) => `/room?roomId=${roomId}?userId=${userId}`,
+  changeRoomPhoto: (userId: string, roomId: string) => `/room-photo?roomId=${roomId}?userId=${userId}`,
+  deleteRoom: (roomId: string) => `/room?roomId=${roomId}`,
+  addUserToRoom: (userId: string, roomId: string, newUserIdentifier: string) =>
+    `/user?userId=${userId}&roomId=${roomId}&newUserIdentifier=${newUserIdentifier}`,
+  deleteUserFromRoom: (userId: string, roomId: string) => `/user?userId=${userId}&roomId=${roomId}&type=DELETE_USER`,
+  changeUserRightsInRoom: (userId: string, roomId: string) => `/user-rights?userId=${userId}&roomId=${roomId}`,
+  loadUserRights: (userId: string, roomId: string) => `/rights?userId=${userId}&roomId=${roomId}`,
+  leaveRoom: (userId: string, roomId: string) => `/user?userId=${userId}&roomId=${roomId}&type=LEAVE_ROOM`,
+  notifications: (userId: string, roomId: string, notifications: string) =>
+    `/notifications?userId=${userId}&roomId=${roomId}&notifications=${notifications}`,
 };
