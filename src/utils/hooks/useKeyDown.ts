44950a7634d1dfb934a3b077f9c2070c4e7da360
Sergey <sergios33033@gmail.com>
2021-08-08 17:56:33 +0300

    Ket down listener hook added.

---
+++ src/utils/hooks/useKeyDown.ts
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+export default function useKeyDown(keyCode: string, onClick: () => void, deps?: any[], mustBeRemoved?: boolean) {
+  useEffect(() => {
+    if (mustBeRemoved) {
+      document.removeEventListener("keydown", handleKeyDown);
+    }
+
+    function handleKeyDown(event: any) {
+      const condition = deps ? deps.every((m) => Boolean(m)) : true;
+      if (condition && event.code === keyCode) {
+        onClick();
+      }
+    }
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [onClick, deps, mustBeRemoved]);
+}
