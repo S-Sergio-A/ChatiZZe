c33e1a8c5350ef3472ce834d3d13b96a9bd378e6
Sergey <sergios33033@gmail.com>
2021-08-08 17:58:54 +0300

    i18next configuration file.

--- src/utils/i18n/i18n.ts
+++ src/utils/i18n/i18n.ts
@@ -1,15 +1,13 @@
-import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import HttpApi from "i18next-http-backend";
 import i18n from "i18next";
 
 i18n
-  .use(I18nextBrowserLanguageDetector)
-  .use(initReactI18next)
   .use(HttpApi)
+  .use(initReactI18next)
   .init({
     backend: {
-      loadPath: "http://localhost:3000/locales/{{lng}}.json",
+      loadPath: "http://localhost:3000/locale/{{lng}}.json",
       allowMultiLoading: true
     },
     load: "languageOnly",
@@ -17,7 +15,8 @@ i18n
     ns: ["common"],
     defaultNS: "common",
     fallbackLng: "en",
-    debug: true,
+    debug: false,
+    keySeparator: false,
     interpolation: {
       escapeValue: false
     },
