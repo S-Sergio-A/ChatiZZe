95d91f33d4d32ecef0c0cbf80dbb5ece480b4f80
Sergey <sergios33033@gmail.com>
2021-08-08 18:48:01 +0300

    The list of routes.

--- src/pages/routes/Routes.tsx
+++ src/pages/routes/Routes.tsx
@@ -1,60 +1,51 @@
-import React, { useContext } from "react";
 import { Route, Switch } from "react-router-dom";
-import { AuthedRoute, UnauthedRoute } from "./Redirect";
-import { AuthContext } from "../../context/auth/AuthContext";
-import Main from "../main-page/Main";
-import Attribution from "../attribution/Attribution";
-import ContactUs from "../contact-us/ContactUs";
-import InformationPage from "../info-pages/InformationPage";
-import PrivacyPolicy from "../info-pages/PrivacyPolicy";
-import CookiePolicy from "../info-pages/CookiePolicy";
-import TermsAndConditions from "../info-pages/TermsAndConditions";
-import Registration from "../registration/Registration";
-import Login from "../login/Login";
-import PageNotFound from "../error/PageNotFound";
-import Chat from "../chat/Chat";
-import FeaturesPage from "../features/Features";
+import { lazy } from "react";
+// import TermsAndConditions from "../info-pages/TermsAndConditions";
+// import InformationPage from "../info-pages/InformationPage";
+// import Registration from "../registration/Registration";
+// import PrivacyPolicy from "../info-pages/PrivacyPolicy";
+// import Verification from "../verification/Verification";
+// import CookiePolicy from "../info-pages/CookiePolicy";
+// import Attribution from "../attribution/Attribution";
+// import PageNotFound from "../error/PageNotFound";
+// import FeaturesPage from "../features/Features";
+// import ContactUs from "../contact-us/ContactUs";
+// import Main from "../main-page/Main";
+// import Login from "../login/Login";
+// import Chat from "../chat/Chat";
+// import Blog from "../blog/Blog";
 
-// const Attribution = lazy(() =>  import(/* webpackChunkName: "attribution" */ '../attribution/Attribution'));
-// const PageNotFound = lazy(() =>  import(/* webpackChunkName: "notFound" */ '../error/PageNotFound'));
-// const CateringController = lazy(() =>
-//   import(/* webpackChunkName: "cateringController", webpackPrefetch: true */ '../catering/CateringController')
-// );
-// const Catering = lazy(() =>  import(/* webpackChunkName: "catering" */ '../catering/Catering'));
-// const News = lazy(() =>  import(/* webpackChunkName: "news" */ '../news/News'));
-// const PressRelease = lazy(() =>  import(/* webpackChunkName: "pressRelease" */ '../news/PressRelease'));
-// const ShopAll = lazy(() =>  import(/* webpackChunkName: "shopAll", webpackPrefetch: true */ '../shop-all/ShopAll'));
-// const ProductsList = lazy(() =>  import(/* webpackChunkName: "prodList", webpackPrefetch: true */ '../shop-all/ProductsList'));
-// const ProductPage = lazy(() =>  import(/* webpackChunkName: "prodPage", webpackPrefetch: true */ '../shop-all/ProductPage'));
-// const Registration = lazy(() => import(/* webpackChunkName: "registration", webpackPrefetch: true */ '../user/Registration'));
-// const EmailValidation = lazy(() => import(/* webpackChunkName: "emailVal", webpackPrefetch: true */ '../user/EmailValidation'));
-// const Login = lazy(() => import(/* webpackChunkName: "login" */ '../login/Login'));
-// const Homepage = lazy(() => import(/* webpackChunkName: "homepage" */ '../user/Homepage'),);
+const Attribution = lazy(() => import(/* webpackChunkName: "attribution" */ "../attribution/Attribution"));
+const PageNotFound = lazy(() => import(/* webpackChunkName: "notFound" */ "../error/PageNotFound"));
+const FeaturesPage = lazy(() => import(/* webpackChunkName: "features" */ "../features/Features"));
+const Registration = lazy(() => import(/* webpackChunkName: "registration", webpackPrefetch: true */ "../registration/Registration"));
+const Verification = lazy(() => import(/* webpackChunkName: "verification", webpackPrefetch: true */ "../verification/Verification"));
+const Login = lazy(() => import(/* webpackChunkName: "login" */ "../login/Login"));
+const Chat = lazy(() => import(/* webpackChunkName: "chat" */ "../chat/Chat"));
+const Blog = lazy(() => import(/* webpackChunkName: "blog" */ "../blog/Blog"));
+const ContactUs = lazy(() => import(/* webpackChunkName: "contact" */ "../contact-us/ContactUs"));
+const CookiePolicy = lazy(() => import(/* webpackChunkName: "cookPol" */ "../info-pages/CookiePolicy"));
+const TermsAndConditions = lazy(() => import(/* webpackChunkName: "termCond" */ "../info-pages/TermsAndConditions"));
+const PrivacyPolicy = lazy(() => import(/* webpackChunkName: "privPol" */ "../info-pages/PrivacyPolicy"));
+const InformationPage = lazy(() => import(/* webpackChunkName: "faq" */ "../info-pages/InformationPage"));
+const Main = lazy(() => import(/* webpackChunkName: "main" */ "../main-page/Main"));
 
 export const Routes = () => {
-  const { logged } = useContext(AuthContext);
-
   return (
     <Switch>
       <Route exact path="/:lang/" component={Main} />
       <Route exact path="/:lang/features" component={FeaturesPage} />
       <Route exact path="/:lang/faq" component={InformationPage} />
-      <Route exact path="/:lang/attribution" component={Attribution} />
-
-      <Route exact path="/:lang/contact-us" component={ContactUs} />
-      <Route exact path="/:lang/chat" component={Chat} />
-
       <Route exact path="/:lang/terms-of-use" component={TermsAndConditions} />
       <Route exact path="/:lang/privacy-policy" component={PrivacyPolicy} />
       <Route exact path="/:lang/cookie-policy" component={CookiePolicy} />
-
+      <Route exact path="/:lang/attribution" component={Attribution} />
+      <Route exact path="/:lang/contact-us" component={ContactUs} />
+      <Route exact path="/:lang/blog" component={Blog} />
       <Route exact path="/:lang/user/registration" component={Registration} />
-      <UnauthedRoute path="/:lang/user/login" component={Login} logged={logged} isPrivate={false} />
-      <UnauthedRoute path="/:lang/chats" component={Chat} logged={logged} isPrivate />
-      {/*<Route exact path="/:lang/user/verification/:email/:verificationCode" component={EmailValidation} />*/}
-      {/*<Route exact path="/:lang/support" component={Support} />*/}
-      {/*<Route exact path="/:lang/blog" component={Blog} />*/}
-      {/*<Route exact path="/:lang/blog/:id" component={PressRelease} />*/}
+      <Route path="/:lang/user/login" component={Login} />
+      <Route exact path="/:lang/verification/:type/:verificationCode" component={Verification} />
+      <Route exact path="/:lang/chat" component={Chat} />
       <Route component={PageNotFound} />
     </Switch>
   );
