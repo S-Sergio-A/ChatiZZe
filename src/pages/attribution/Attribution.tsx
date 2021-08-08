26dbcf9de4d637da2c48d6a9f053ff62249514fa
Sergey <sergios33033@gmail.com>
2021-08-08 18:46:20 +0300

    An attribution page.

--- src/pages/attribution/Attribution.tsx
+++ src/pages/attribution/Attribution.tsx
@@ -1,60 +1,40 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
-import { iconsAttributionList, photosAttributionList } from './attributionList';
-import './Attribution.css';
+import { useTranslation } from "react-i18next";
+import { iconsAttributionList } from "./attributionList";
+import "./Attribution.css";
+import Head from "../../components/head/Head";
+import React from "react";
 
 export default function Attribution() {
-  const [ t ] = useTranslation();
-
+  const [t] = useTranslation();
+  
   return (
-    <div className="Attr-Page Grid Nunito">
-      <header className="B-T T-C flex J-C-C a-i-c f-f-c-n F-W">
-        <h1 className="h3-size">
-          {t('attribution.header')}
-        </h1>
-        <h2 className="h5-size">
-          {t('attribution.main')}
-          <a className="h6-size" href="mailto:sergiom33033@gmail.com">
-            {' sergiom33033@gmail.com'}
+    <main id="main" className="attr-page grid">
+      <Head title={t("attr.seo.title")} description={t("attr.seo.description")} />
+      <header className="t-b flex j-c-c a-i-c f-f-c-n f-w">
+        <h1 className="h2-s t-c">{t("attribution.header")}</h1>
+        <h2 className="h5-s t-c">
+          {t("attribution.main")}
+          <a className="h6-s" href="mailto:sergiom33033@gmail.com">
+            {" sergiom33033@gmail.com"}
           </a>
         </h2>
       </header>
-      <div className="B-M flex J-C-C a-i-c f-f-c-n F-W">
-        <section className="flex J-C-C a-i-c f-f-c-n F-W">
-          <header className="T-C F-W">
-            <h3>
-              {t('icons')}
-            </h3>
+      <div className="m-b flex j-c-c a-i-c f-f-c-n f-w">
+        <section className="flex j-c-c a-i-c f-f-c-n f-w">
+          <header>
+            <h3>{t("icons")}</h3>
           </header>
-          <ul className="flex a-i-c J-C-C f-f-c-n F-W">
+          <ul className="flex a-i-c j-c-c f-f-c-n f-w">
             {iconsAttributionList.map((item, index) => (
-              <li key={index} className="F-W T-L">
+              <li key={index} className="f-w T-L">
                 <a href={item.link} title={item?.title}>
                   {item.author}
                 </a>
               </li>
-              )
-            )}
-          </ul>
-        </section>
-        <section className="flex J-C-C a-i-c f-f-c-n F-W">
-          <header className="T-C F-W">
-            <h3>
-              {t('photos')}
-            </h3>
-          </header>
-          <ul className="flex a-i-c J-C-C f-f-c-n F-W">
-            {photosAttributionList.map((item, index) => (
-              <li key={index} className="F-W T-L">
-                <a href={item}>
-                  {item}
-                </a>
-              </li>
-              )
-            )}
+            ))}
           </ul>
         </section>
       </div>
-    </div>
+    </main>
   );
 }
