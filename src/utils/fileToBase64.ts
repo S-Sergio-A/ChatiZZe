e1c9ffbf6fddb7a8cfd3c1cb62f72b887b8bd27a
Sergey <sergios33033@gmail.com>
2021-08-09 13:50:39 +0300

    Add function that converts a file to base64 string.

---
+++ src/utils/fileToBase64.ts
@@ -0,0 +1,7 @@
+export const fileToBase64 = async (file: File) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (e) => reject(e);
+  });
